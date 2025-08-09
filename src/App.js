edcce9bc102eed380b9686e6719331cdd3da3605
nishant2narula <nishant2narula@gmail.com>
2025-08-09 17:24:20 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ToDo from "./components/ToDo";
+
+function App() {
+  return <div>
+  <h1>TO DO LIST</h1> 
+  <ToDo text='Learn React'  />
+  <ToDo text='Master React' />
+  <ToDo text='Explore React' />
+  </div>;
+}
+
+export default App;
